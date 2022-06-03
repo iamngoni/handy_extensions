748761eaf24e9c5e5955dda5dc6a86f5bd4b8c50
iamngoni <imngonii@gmail.com>
2022-06-03 10:31:01 +0200

    string and iteration

--- lib/src/string.dart
+++ lib/src/string.dart
@@ -6,4 +6,11 @@ extension HandyStringExtension on String {
         characters.map((String char) => 127397 + char.codeUnits.first);
     return String.fromCharCodes(characterCodes);
   }
+
+  /// Return a new string with first character as upper case.
+  String get titleCase {
+    if (length <= 1) return toUpperCase();
+
+    return '${this[0].toUpperCase()}${substring(1).toLowerCase()}';
+  }
 }
